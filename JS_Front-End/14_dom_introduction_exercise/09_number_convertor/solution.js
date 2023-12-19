cf415407448b9b7a3d199e372c832710a9887153
Yavor Gornalov <y.gornalov@gmail.com>
2023-12-19 10:56:44 +0200

    09. Number Convertor --uploaded

--- JS_Front-End/14_dom_introduction_exercise/09_number_convertor/solution.js
+++ JS_Front-End/14_dom_introduction_exercise/09_number_convertor/solution.js
@@ -1,4 +1,26 @@
 function solve() {
+    let selectToElement = document.getElementById("selectMenuTo");
+    const optionToBinary = document.createElement("option");
+    optionToBinary.value = "binary";
+    optionToBinary.textContent = "Binary";
+    selectToElement.appendChild(optionToBinary);
 
-    //TODO...
+    const optionToHexadecimal = document.createElement("option");
+    optionToHexadecimal.value = "hexadecimal";
+    optionToHexadecimal.textContent = "Hexadecimal";
+    selectToElement.appendChild(optionToHexadecimal);
+
+    const button = document.getElementsByTagName("button")[0];
+    button.addEventListener("click", convert);
+    function convert(e) {
+        let decimal = parseInt(document.getElementById("input").value);
+        let selectedOptionTo = document.getElementById("selectMenuTo").value;
+        let outputElement = document.getElementById("result");
+
+        if (selectedOptionTo === "binary") {
+            outputElement.value = decimal.toString(2);
+        } else if (selectedOptionTo === "hexadecimal") {
+            outputElement.value = decimal.toString(16).toLocaleUpperCase();
+        }
+    }
 }

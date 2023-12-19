20745ce8d5e582420bff17b1cf5498e91b7092d5
Yavor Gornalov <y.gornalov@gmail.com>
2023-12-19 10:01:48 +0200

    08. Generate Report --uploaded

--- JS_Front-End/14_dom_introduction_exercise/08_generate_report/report.js
+++ JS_Front-End/14_dom_introduction_exercise/08_generate_report/report.js
@@ -1,3 +1,29 @@
 function generateReport() {
-    //TODO
+    let columns = Array.from(document.querySelectorAll("thead tr th input"));
+
+    let selectedColumns = {};
+    let idx = 0;
+    columns.forEach((col) => {
+        if (col.checked) {
+            selectedColumns[idx] = col.parentElement.textContent.trim().toLowerCase();
+        }
+        idx++;
+    });
+
+    let selectedContent = [];
+    let rows = Array.from(document.querySelectorAll("tbody tr"));
+    rows.forEach((row) => {
+        let cells = row.children;
+        let selectedRowContent = {};
+
+        for (let i = 0; i < columns.length; i++) {
+            if (selectedColumns.hasOwnProperty(i)) {
+                selectedRowContent[selectedColumns[i]] = cells[i].textContent;
+            }
+        }
+        selectedContent.push(selectedRowContent);
+    });
+
+    let outputElement = document.getElementById("output");
+    outputElement.textContent = JSON.stringify(selectedContent, null, 4);
 }

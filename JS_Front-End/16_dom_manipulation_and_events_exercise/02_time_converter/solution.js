f3ce46a2cf834fdbb57a49987c86de0aec72f575
Yavor Gornalov <y.gornalov@gmail.com>
2023-12-21 18:07:16 +0200

    02. Time Converter solved with mapper

--- JS_Front-End/16_dom_manipulation_and_events_exercise/02_time_converter/solution.js
+++ JS_Front-End/16_dom_manipulation_and_events_exercise/02_time_converter/solution.js
@@ -1,4 +1,28 @@
 function attachEventsListeners() {
+    const TIME_ARRAY = [1, 24, 1440, 86400];
+    const mapper = {
+        days: TIME_ARRAY,
+        hours: TIME_ARRAY.map((num) => num / TIME_ARRAY[1]),
+        minutes: TIME_ARRAY.map((num) => num / TIME_ARRAY[2]),
+        seconds: TIME_ARRAY.map((num) => num / TIME_ARRAY[3]),
+    };
 
-    console.log('TODO:...');
+    let buttons = Array.from(document.querySelectorAll("div>input:last-child"));
+    let dataFields = Array.from(document.querySelectorAll("div>input:nth-child(2)"));
+
+    buttons.forEach((button) => {
+        button.addEventListener("click", convert);
+    });
+
+    function convert() {
+        const key = this.id.replace("Btn", "");
+        const value = Number(document.getElementById(key).value);
+
+        if (!value) return;
+
+        for (let idx = 0; idx < dataFields.length; idx++) {
+            const field = dataFields[idx];
+            field.value = mapper[key][idx] * value;
+        }
+    }
 }

f6795992a57c18593d84ea37b8c702b7c85eb4d1
Yavor Gornalov <y.gornalov@gmail.com>
2023-12-21 14:53:48 +0200

    Problem 01. Sections --uploaded

--- JS_Front-End/16_dom_manipulation_and_events_exercise/01_sections/script.js
+++ JS_Front-End/16_dom_manipulation_and_events_exercise/01_sections/script.js
@@ -1,3 +1,18 @@
 function create(words) {
-   console.log('TODO:...');
+    let parentDiv = document.getElementById("content");
+    words.forEach((word) => {
+        let innerDiv = document.createElement("div");
+        innerDiv.addEventListener("click", showParagraph);
+
+        let pElement = document.createElement("p");
+        pElement.textContent = word;
+        pElement.style.display = "none";
+
+        innerDiv.appendChild(pElement);
+        parentDiv.appendChild(innerDiv);
+
+        function showParagraph(e) {
+            pElement.style.display = "block";
+        }
+    });
 }

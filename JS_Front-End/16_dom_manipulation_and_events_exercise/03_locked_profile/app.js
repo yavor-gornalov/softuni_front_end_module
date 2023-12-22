d6edb52eb07bce18ec806eca0c698d5c8cd55c0f
Yavor Gornalov <y.gornalov@gmail.com>
2023-12-22 08:43:22 +0200

    03. Locked Profile --uploaded

--- JS_Front-End/16_dom_manipulation_and_events_exercise/03_locked_profile/app.js
+++ JS_Front-End/16_dom_manipulation_and_events_exercise/03_locked_profile/app.js
@@ -1,3 +1,23 @@
 function lockedProfile() {
-    console.log('TODO...')
+    const showHideButtons = Array.from(document.querySelectorAll("div[class=profile]>button"));
+
+    showHideButtons.forEach((button) => {
+        button.addEventListener("click", showHideInfo);
+    });
+
+    function showHideInfo(e) {
+        const radioButtons = Array.from(this.parentNode.querySelectorAll("input[type=radio]"));
+        const isUserLocked = radioButtons.filter((radio) => radio.checked)[0].value;
+        const hiddenInfo = this.parentNode.querySelector("div[id$='HiddenFields']");
+
+        if (isUserLocked === "unlock") {
+            if (this.textContent === "Show more") {
+                hiddenInfo.style.display = "block";
+                this.textContent = "Hide it";
+            } else {
+                hiddenInfo.style.display = "";
+                this.textContent = "Show more";
+            }
+        }
+    }
 }

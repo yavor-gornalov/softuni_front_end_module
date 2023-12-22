226b147db41908ed75a21ea88f787b6d6566e019
Yavor Gornalov <y.gornalov@gmail.com>
2023-12-22 09:04:12 +0200

    04. Fill Dropdown --uploaded

--- JS_Front-End/16_dom_manipulation_and_events_exercise/04_fill_dropdown/dropdown.js
+++ JS_Front-End/16_dom_manipulation_and_events_exercise/04_fill_dropdown/dropdown.js
@@ -1,3 +1,14 @@
 function addItem() {
-    console.log('TODO:...');
+    const select = document.getElementById("menu");
+    let itemText = document.getElementById("newItemText");
+    let itemValue = document.getElementById("newItemValue");
+
+    newOption = document.createElement("option");
+    newOption.textContent = itemText.value;
+    newOption.value = itemValue.value;
+
+    select.appendChild(newOption);
+
+    itemText.value = "";
+    itemValue.value = "";
 }

926d4dbedfc241a8edb8f0ae1f03287fcfed1bfe
Yavor Gornalov <y.gornalov@gmail.com>
2023-12-22 10:56:49 +0200

    Problem 05. Encode and Decode Messages --solved

--- JS_Front-End/16_dom_manipulation_and_events_exercise/05_encode_and_decode_messages/app.js
+++ JS_Front-End/16_dom_manipulation_and_events_exercise/05_encode_and_decode_messages/app.js
@@ -1,3 +1,20 @@
 function encodeAndDecodeMessages() {
-    console.log('TODO...')
+    const [encodeButton, decodeButton] = Array.from(document.querySelectorAll("div>button"));
+    const [messageArea, encodedMessageArea] = Array.from(document.querySelectorAll("div>textarea"));
+    let rawMessage = "";
+    encodeButton.addEventListener("click", () => {
+        rawMessage = messageArea.value;
+        let encodedMessage = "";
+        for (const idx in rawMessage) {
+            let charCode = rawMessage.charCodeAt(idx) + 1;
+            encodedMessage += String.fromCharCode(charCode);
+        }
+
+        messageArea.value = "";
+        encodedMessageArea.value = encodedMessage;
+    });
+
+    decodeButton.addEventListener("click", () => {
+        encodedMessageArea.value = rawMessage;
+    });
 }

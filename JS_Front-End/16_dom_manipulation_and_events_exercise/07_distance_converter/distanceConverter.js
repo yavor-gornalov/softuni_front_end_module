d03ccedb610e4f84b0c660aaac847135c36c3bc7
Yavor Gornalov <y.gornalov@gmail.com>
2023-12-23 07:59:39 +0200

    07. Distance Converter -- uploaded

--- JS_Front-End/16_dom_manipulation_and_events_exercise/07_distance_converter/distanceConverter.js
+++ JS_Front-End/16_dom_manipulation_and_events_exercise/07_distance_converter/distanceConverter.js
@@ -1,3 +1,24 @@
 function attachEventsListeners() {
-    console.log('TODO:...');
+    const UNITS_MAP = {
+        // units: meters
+        km: 1000,
+        m: 1,
+        cm: 0.01,
+        mm: 0.001,
+        mi: 1609.34,
+        yrd: 0.9144,
+        ft: 0.3048,
+        in: 0.0254,
+    };
+
+    const convertButton = document.getElementById("convert");
+    convertButton.addEventListener("click", () => {
+        const inputDistance = Number(document.getElementById("inputDistance").value);
+        const inputUnits = document.getElementById("inputUnits").value;
+        const outputUnits = document.getElementById("outputUnits").value;
+        const outputDistanceElement = document.getElementById("outputDistance");
+
+        const result = (UNITS_MAP[inputUnits] * inputDistance) / UNITS_MAP[outputUnits];
+        outputDistanceElement.value = result;
+    });
 }
